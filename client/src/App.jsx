9b491b3fb70e68816bb4401cc5d4ca3781222344
Suraj Kasaudhan <kasaudhansuraj25@gmail.com>
2025-06-10 10:51:21 +0530

    djhksjisu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard.jsx";
 import PerInfo from "./pages/AddEmployee/PerInfo.jsx";
-import employees from "./pages/Dashboard/Employee.jsx"; // Assuming this is an array
+import employees from "./pages/Dashboard/Employee.jsx";
 import EmployeeDetWrapper from "./pages/Dashboard/EmployeeDetWrapper.jsx";
 import AllDept from "./pages/Department/AllDept.jsx";
 import DepartmentDetail from "./pages/Department/DepartmentDetail.jsx";
@@ -10,41 +10,36 @@ import Jobs from "./pages/Jobs/Jobs.jsx";
 import Candidates from "./pages/Candidates/Candidates.jsx";
 import Holidays from "./pages/Holidays/Holidays.jsx";
 import Navbar from "./pages/Navbar/Navbar.jsx";
-import { Settings } from "lucide-react";
 import Setting from "./pages/Settings/Setting.jsx";
 import Notification from "./pages/Notification/Notification.jsx";
 
 function App() {
   return (
     <HashRouter>
-            {/* <Navbar /> */}
+      {/* Navbar is optional, uncomment if needed */}
+      {/* <Navbar /> */}
       <main className="w-full min-h-screen">
-      
         <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/DexKor_HRMS" element={<Dashboard />} />
-        {/* <Route path="/DexKor_HRMS/Navbar" element={<Navbar />} /> */}
+          {/* Root route (when user lands at base path) */}
+          <Route path="/" element={<Dashboard />} />
 
-          {/* Department Routes */}
-          <Route path="/DexKor_HRMS/#/allDepartment" element={<AllDept />} />
-          <Route
-  path="/DexKor_HRMS/#/allDepartment/department/:name"
-  element={<DepartmentDetail />}
-/>
-<Route path="/DexKor_HRMS/#/Attendance" element={<Attendance/>} />
-<Route path="/DexKor_HRMS/#/Jobs" element={<Jobs/>} />
-<Route path="/DexKor_HRMS/#/Candidates" element={<Candidates/>} />
-<Route path="/DexKor_HRMS/#/Holidays" element={<Holidays/>} />
-<Route path="/DexKor_HRMS/#/Setting" element={<Setting/>} />
-<Route path="/DexKor_HRMS/#/Notification" element={<Notification/>} />
+          {/* App Routes */}
+          <Route path="/DexKor_HRMS" element={<Dashboard />} />
+          <Route path="/DexKor_HRMS/allDepartment" element={<AllDept />} />
+          <Route path="/DexKor_HRMS/allDepartment/department/:name" element={<DepartmentDetail />} />
+          <Route path="/DexKor_HRMS/Attendance" element={<Attendance />} />
+          <Route path="/DexKor_HRMS/Jobs" element={<Jobs />} />
+          <Route path="/DexKor_HRMS/Candidates" element={<Candidates />} />
+          <Route path="/DexKor_HRMS/Holidays" element={<Holidays />} />
+          <Route path="/DexKor_HRMS/Setting" element={<Setting />} />
+          <Route path="/DexKor_HRMS/Notification" element={<Notification />} />
 
+          {/* Employee routes */}
+          <Route path="/add-employee-personal" element={<PerInfo />} />
+          <Route path="/employee/:id" element={<EmployeeDetWrapper employees={employees} />} />
 
-          {/* Employee Add + Detail View */}
-          <Route path="/#/add-employee-personal" element={<PerInfo />} />
-          <Route
-            path="/#/employee/:id"
-            element={<EmployeeDetWrapper employees={employees} />}
-          />
+          {/* Optional fallback */}
+          <Route path="*" element={<Dashboard />} />
         </Routes>
       </main>
     </HashRouter>
